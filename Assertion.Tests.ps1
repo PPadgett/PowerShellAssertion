73da6db1cac53e79067ceb134c354b41d3bb4576
PPadgett <Preston.Padgett@outlook.com>
2023-07-14 22:11:39 -0500

    Update Assertion.Tests.ps1

--- Assertion.Tests.ps1
+++ Assertion.Tests.ps1
@@ -2,15 +2,12 @@
     . $PSCommandPath.Replace('.Tests.ps1', '.ps1')
 }
 
-# Load the functions to be tested
-. .\Assertion.ps1
-
 # Test cases
 Describe 'Assert-Condition' {
     Context 'When DebugPreference is not SilentlyContinue and the condition is true' {
         It 'Should output the provided message' {
             Mock -CommandName 'Get-Variable' -MockWith { return 'Continue' } -ParameterFilter { $Name -eq 'DebugPreference' }
-            { Assert-Condition -Condition $true -Message 'Assertion failed: Test error message' } |
+            Assert-Condition -Condition $true -Message 'Assertion failed: Test error message' |
                 Should -Contain 'Assertion failed:'
         }
     }
@@ -18,7 +15,7 @@ Describe 'Assert-Condition' {
     Context 'When DebugPreference is SilentlyContinue and the condition is true' {
         It 'Should not output message' {
             Mock -CommandName 'Get-Variable' -MockWith { return 'SilentlyContinue' } -ParameterFilter { $Name -eq 'DebugPreference' }
-            { Assert-Condition -Condition $true -Message 'Assertion failed: Test error message' } |
+            Assert-Condition -Condition $true -Message 'Assertion failed: Test error message' |
                 Should -Be $null
         }
     }
@@ -40,7 +37,7 @@ Describe 'Get-Square' {
     Context 'When the input number is negative' {
         It 'Should output an message stating that the Assertion failed: input must not be a negative number' {
             Mock -CommandName 'Get-Variable' -MockWith { return 'Continue' } -ParameterFilter { $Name -eq 'DebugPreference' }
-            { Get-Square -num -4 } |
+            Get-Square -num -4 |
                 Should -Contain 'Assertion failed:'
         }
     }
